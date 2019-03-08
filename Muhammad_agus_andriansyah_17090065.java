f4070654586dc58bdf02ecc90fc3650f0b930c97
MuhammadAgusAndriansyah <48339373+MuhammadAgusAndriansyah@users.noreply.github.com>
2019-03-08 08:52:35 +0700

    Add files via upload

---
+++ Muhammad_agus_andriansyah_17090065.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author GL62M-7REX
+ */
+public class Muhammad_agus_andriansyah_17090065 {
+        public static void main(String[] args) {
+        System.out.println("Nama    :Muhammad Agus Andriansyah");
+        System.out.println("Nim     :17090065");
+        System.out.println("Kelas   :4A");
+        // TODO code application logic here
+    }
+}
